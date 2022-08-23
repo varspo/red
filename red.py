ee801b70db81a94a381822eb0185941d07739bd9
Varun Soni <74827745+varspo@users.noreply.github.com>
2022-08-23 20:34:59 +0530

    red is completely built

---
+++ red.py
@@ -0,0 +1,382 @@
+from __future__ import print_function
+import re, sys, time
+from itertools import count
+from collections import namedtuple
+
+piece = { 'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 60000 }
+positionTable = {
+    'P': (   0,   0,   0,   0,   0,   0,   0,   0,
+            78,  83,  86,  73, 102,  82,  85,  90,
+             7,  29,  21,  44,  40,  31,  44,   7,
+           -17,  16,  -2,  15,  14,   0,  15, -13,
+           -26,   3,  10,   9,   6,   1,   0, -23,
+           -22,   9,   5, -11, -10,  -2,   3, -19,
+           -31,   8,  -7, -37, -36, -14,   3, -31,
+             0,   0,   0,   0,   0,   0,   0,   0),
+    'N': ( -66, -53, -75, -75, -10, -55, -58, -70,
+            -3,  -6, 100, -36,   4,  62,  -4, -14,
+            10,  67,   1,  74,  73,  27,  62,  -2,
+            24,  24,  45,  37,  33,  41,  25,  17,
+            -1,   5,  31,  21,  22,  35,   2,   0,
+           -18,  10,  13,  22,  18,  15,  11, -14,
+           -23, -15,   2,   0,   2,   0, -23, -20,
+           -74, -23, -26, -24, -19, -35, -22, -69),
+    'B': ( -59, -78, -82, -76, -23,-107, -37, -50,
+           -11,  20,  35, -42, -39,  31,   2, -22,
+            -9,  39, -32,  41,  52, -10,  28, -14,
+            25,  17,  20,  34,  26,  25,  15,  10,
+            13,  10,  17,  23,  17,  16,   0,   7,
+            14,  25,  24,  15,   8,  25,  20,  15,
+            19,  20,  11,   6,   7,   6,  20,  16,
+            -7,   2, -15, -12, -14, -15, -10, -10),
+    'R': (  35,  29,  33,   4,  37,  33,  56,  50,
+            55,  29,  56,  67,  55,  62,  34,  60,
+            19,  35,  28,  33,  45,  27,  25,  15,
+             0,   5,  16,  13,  18,  -4,  -9,  -6,
+           -28, -35, -16, -21, -13, -29, -46, -30,
+           -42, -28, -42, -25, -25, -35, -26, -46,
+           -53, -38, -31, -26, -29, -43, -44, -53,
+           -30, -24, -18,   5,  -2, -18, -31, -32),
+    'Q': (   6,   1,  -8,-104,  69,  24,  88,  26,
+            14,  32,  60, -10,  20,  76,  57,  24,
+            -2,  43,  32,  60,  72,  63,  43,   2,
+             1, -16,  22,  17,  25,  20, -13,  -6,
+           -14, -15,  -2,  -5,  -1, -10, -20, -22,
+           -30,  -6, -13, -11, -16, -11, -16, -27,
+           -36, -18,   0, -19, -15, -15, -21, -38,
+           -39, -30, -31, -13, -31, -36, -34, -42),
+    'K': (   4,  54,  47, -99, -99,  60,  83, -62,
+           -32,  10,  55,  56,  56,  55,  10,   3,
+           -62,  12, -57,  44, -67,  28,  37, -31,
+           -55,  50,  11,  -4, -19,  13,   0, -49,
+           -55, -43, -52, -28, -51, -47,  -8, -50,
+           -47, -42, -43, -79, -64, -32, -29, -32,
+            -4,   3, -14, -50, -57, -18,  13,   4,
+            17,  30,  -3, -14,   6,  -1,  40,  18),
+}
+
+for k, table in positionTable.items():
+    padrow = lambda row: (0,) + tuple(x+piece[k] for x in row) + (0,)
+    positionTable[k] = sum((padrow(table[i*8:i*8+8]) for i in range(8)), ())
+    positionTable[k] = (0,)*20 + positionTable[k] + (0,)*20
+    print(positionTable['P'])
+# The board is represented using a FEN string. 
+# This is a standard across the engines I've looked at.
+# About FEN: https://bit.ly/3A8ulUc (chess.com)
+
+A1, H1, A8, H8 = 91, 98, 21, 28
+
+initial = (
+    '         \n'  #   0 -  9
+    '         \n'  #  10 - 19
+    ' rnbqkbnr\n'  #  20 - 29
+    ' pppppppp\n'  #  30 - 39
+    ' ........\n'  #  40 - 49
+    ' ........\n'  #  50 - 59
+    ' ........\n'  #  60 - 69
+    ' ........\n'  #  70 - 79
+    ' PPPPPPPP\n'  #  80 - 89
+    ' RNBQKBNR\n'  #  90 - 99
+    '         \n'  # 100 -109
+    '         \n'  # 110 -119
+)
+
+# Lists of allowed movement for each piece
+N, E, S, W = -10, 1, 10, -1
+directions = {
+    'P': (N, N+N, N+W, N+E),
+    'N': (N+N+E, E+N+E, E+S+E, S+S+E, S+S+W, W+S+W, W+N+W, N+N+W),
+    'B': (N+E, S+E, S+W, N+W),
+    'R': (N, E, S, W),
+    'Q': (N, E, S, W, N+E, S+E, S+W, N+W),
+    'K': (N, E, S, W, N+E, S+E, S+W, N+W)
+}
+
+# Mate value is set to be greater than 8*queen + 2*(rook+knight+bishop)
+# This is because the condition of a position not being mate with the maximum
+# amount of pieces left is when the opponent has all their pawns promote to queen
+MATE_LOWER = piece['K'] - 10*piece['Q']
+MATE_UPPER = piece['K'] + 10*piece['Q']
+
+# The table size is the maximum number of elements in the transposition table.
+# About transpositions: https://bit.ly/3AgAgqf (wiki); https://bit.ly/3Kf8BL0 (chess.com)
+TABLE_SIZE = 1e7
+
+# Constants for tuning the search
+QS_LIMIT = 219
+EVAL_ROUGHNESS = 13
+DRAW_TEST = True
+
+
+# The algorithm starts here
+class Position(namedtuple('Position', 'board score wc bc ep kp')):
+    # board -> a 120 char representation of the board
+    # score -> the board evaluation
+    # wc -> the castling rights
+    # bc -> the opponent castling rights
+    # ep -> the en passant square
+    # kp -> the castling square
+
+    def gen_moves(self):
+        # This is trivial. Keep moving until a piece is hit by some other piece.
+        # If the piece is ours, stop; otherwise, it can be captured.
+        for i, p in enumerate(self.board):
+            if not p.isupper(): continue
+            for d in directions[p]:
+                for j in count(i+d, d):
+                    q = self.board[j]
+
+                    if q.isspace() or q.isupper(): break
+                    # Pawn can move two ranks forward if placed on the starting position
+                    if p == 'P' and d in (N, N+N) and q != '.': break
+                    if p == 'P' and d == N+N and (i < A1+N or self.board[i+N] != '.'): break
+                    if p == 'P' and d in (N+W, N+E) and q == '.' \
+                            and j not in (self.ep, self.kp, self.kp-1, self.kp+1): break
+                    yield (i, j)
+
+                    if p in 'PNK' or q.islower(): break
+                    # Castling
+                    if i == A1 and self.board[j+E] == 'K' and self.wc[0]: yield (j+E, j+W)
+                    if i == H1 and self.board[j+W] == 'K' and self.wc[1]: yield (j+W, j+E)
+    
+    # Rotate the board
+    def rotate(self):
+        return Position(
+            self.board[::-1].swapcase(), -self.score, self.bc, self.wc,
+            119-self.ep if self.ep else 0,
+            119-self.kp if self.kp else 0)
+    
+    # Clear en passant and castling squares
+    def nullmove(self):
+        return Position(
+            self.board[::-1].swapcase(), -self.score,
+            self.bc, self.wc, 0, 0)
+
+    def move(self, move):
+        i, j = move
+        p, q = self.board[i], self.board[j]
+        put = lambda board, i, p: board[:i] + p + board[i+1:]
+
+        board = self.board
+        wc, bc, ep, kp = self.wc, self.bc, 0, 0
+        score = self.score + self.value(move)
+
+        # Moves
+        board = put(board, j, board[i])
+        board = put(board, i, '.')
+        
+        # Represents the castling rights
+        if i == A1: wc = (False, wc[1])
+        if i == H1: wc = (wc[0], False)
+        if j == A8: bc = (bc[0], False)
+        if j == H8: bc = (False, bc[1])
+
+        # Castling
+        if p == 'K':
+            wc = (False, False)
+            if abs(j-i) == 2:
+                kp = (i+j)//2
+                board = put(board, A1 if j < i else H1, '.')
+                board = put(board, kp, 'R')
+
+        # Pawn promotion, double move and en passant capture
+        # The pawn in red doesn't promote to minor pieces as of now
+        if p == 'P':
+            if A8 <= j <= H8:
+                board = put(board, j, 'Q')
+            if j - i == 2*N:
+                ep = i + N
+            if j == self.ep:
+                board = put(board, j+S, '.')
+
+        return Position(board, score, wc, bc, ep, kp).rotate()
+
+    def value(self, move):
+        i, j = move
+        p, q = self.board[i], self.board[j]
+
+        # Move
+        score = positionTable[p][j] - positionTable[p][i]
+
+        # Capture
+        if q.islower():
+            score += positionTable[q.upper()][119-j]
+
+        # Castling Check
+        if abs(j-self.kp) < 2:
+            score += positionTable['K'][119-j]
+
+        # Castling
+        if p == 'K' and abs(i-j) == 2:
+            score += positionTable['R'][(i+j)//2]
+            score -= positionTable['R'][A1 if j < i else H1]
+
+        # Special pawn moves, such as en passant and promotion
+        if p == 'P':
+            if A8 <= j <= H8:
+                score += positionTable['Q'][j] - positionTable['P'][j]
+            if j == self.ep:
+                score += positionTable['P'][119-(j+S)]
+        return score
+
+# Searching Algorithm
+Entry = namedtuple('Entry', 'lower upper')
+
+class Searcher:
+    def __init__(self):
+        self.tp_score = {}
+        self.tp_move = {}
+        self.history = set()
+        self.nodes = 0
+
+    def bound(self, pos, gamma, depth, root=True):
+        self.nodes += 1
+
+        # If the behavior doesn't change with increasing depth, it's likely the there are no better moves even if depth is increased
+        depth = max(depth, 0)
+
+        # red doesn't detect a checkmate. The game is over when the king is captured.
+        # The king is allowed to move to a square where it's been attacked. Stalemates are checked separately.
+        if pos.score <= -MATE_LOWER:
+            return -MATE_UPPER
+
+        # Detection of three-fold repetion
+        # https://bit.ly/3Coi94o (chess.com)
+        if DRAW_TEST:
+            if not root and pos in self.history:
+                return 0
+
+        entry = self.tp_score.get((pos, depth, root), Entry(-MATE_UPPER, MATE_UPPER))
+        if entry.lower >= gamma and (not root or self.tp_move.get(pos) is not None):
+            return entry.lower
+        if entry.upper < gamma:
+            return entry.upper
+
+        # Move generation
+        def moves():
+            if depth > 0 and not root and any(c in pos.board for c in 'RBNQ'):
+                yield None, -self.bound(pos.nullmove(), 1-gamma, depth-3, root=False)
+            
+            # Zugzwang
+            if depth == 0:
+                yield None, pos.score
+
+            killer = self.tp_move.get(pos)
+            if killer and (depth > 0 or pos.value(killer) >= QS_LIMIT):
+                yield killer, -self.bound(pos.move(killer), 1-gamma, depth-1, root=False)
+
+            for move in sorted(pos.gen_moves(), key=pos.value, reverse=True):
+                if depth > 0 or pos.value(move) >= QS_LIMIT:
+                    yield move, -self.bound(pos.move(move), 1-gamma, depth-1, root=False)
+
+        best = -MATE_UPPER
+        for move, score in moves():
+            best = max(best, score)
+            if best >= gamma:
+                if len(self.tp_move) > TABLE_SIZE: self.tp_move.clear()
+                self.tp_move[pos] = move
+                break
+
+        # Stalemate Check
+        if best < gamma and best < 0 and depth > 0:
+            is_dead = lambda pos: any(pos.value(m) >= MATE_LOWER for m in pos.gen_moves())
+            if all(is_dead(pos.move(m)) for m in pos.gen_moves()):
+                in_check = is_dead(pos.nullmove())
+                best = -MATE_UPPER if in_check else 0
+
+        if len(self.tp_score) > TABLE_SIZE: self.tp_score.clear()
+
+        if best >= gamma:
+            self.tp_score[pos, depth, root] = Entry(best, entry.upper)
+        if best < gamma:
+            self.tp_score[pos, depth, root] = Entry(entry.lower, best)
+
+        return best
+
+    def search(self, pos, history=()):
+        # alpha-beta pruning
+        self.nodes = 0
+        if DRAW_TEST:
+            self.history = set(history)
+            self.tp_score.clear()
+
+        for depth in range(1, 1000):
+            lower, upper = -MATE_UPPER, MATE_UPPER
+            while lower < upper - EVAL_ROUGHNESS:
+                gamma = (lower+upper+1)//2
+                score = self.bound(pos, gamma, depth)
+                if score >= gamma:
+                    lower = score
+                if score < gamma:
+                    upper = score
+
+            self.bound(pos, lower, depth)
+            # If the game hasn't ended yet, we look through the transposition table.
+            yield depth, self.tp_move.get(pos), self.tp_score.get((pos, depth, True)).lower
+
+
+# CLI Interface
+if sys.version_info[0] == 2:
+    input = raw_input
+
+
+def parse(c):
+    fil, rank = ord(c[0]) - ord('a'), int(c[1]) - 1
+    return A1 + fil - 10*rank
+
+
+def render(i):
+    rank, fil = divmod(i - A1, 10)
+    return chr(fil + ord('a')) + str(-rank + 1)
+
+
+def print_pos(pos):
+    print()
+    uni_pieces = {'R':'♜', 'N':'♞', 'B':'♝', 'Q':'♛', 'K':'♚', 'P':'♟',
+                  'r':'♖', 'n':'♘', 'b':'♗', 'q':'♕', 'k':'♔', 'p':'♙', '.':'·'}
+    for i, row in enumerate(pos.board.split()):
+        print(' ', 8-i, ' '.join(uni_pieces.get(p, p) for p in row))
+    print('    a b c d e f g h \n\n')
+
+
+def main():
+    hist = [Position(initial, 0, (True,True), (True,True), 0, 0)]
+    searcher = Searcher()
+    while True:
+        print_pos(hist[-1])
+
+        if hist[-1].score <= -MATE_LOWER:
+            print("You lost")
+            break
+
+        move = None
+        while move not in hist[-1].gen_moves():
+            match = re.match('([a-h][1-8])'*2, input('Your move: '))
+            if match:
+                move = parse(match.group(1)), parse(match.group(2))
+            else:
+                print("Please enter a valid move")
+        hist.append(hist[-1].move(move))
+
+        # After our move we rotate the board and print it again.
+        print_pos(hist[-1].rotate())
+
+        if hist[-1].score <= -MATE_LOWER:
+            print("You won")
+            break
+
+        start = time.time()
+        for _depth, move, score in searcher.search(hist[-1], hist):
+            if time.time() - start > 1:
+                break
+
+        if score == MATE_UPPER:
+            print("Checkmate!")
+
+        # We rotate the board again.
+        print("My move:", render(119-move[0]) + render(119-move[1]))
+        hist.append(hist[-1].move(move))
+
+
+if __name__ == '__main__':
+    main()
